27f32baf0b158ff1cee0f1f672d7a726b5e59a03
unknown <innovationjam9@gmail.com>
2026-03-25 17:00:34 +0530

    more Test Cases

--- playwright.config.ts
+++ playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
 //  reporter: 'html',
-   reporter: [
+reporter: [
   ['line'],
   ['html', { outputFolder: 'playwright-report', open: 'never' }],
   ['junit', { outputFile: 'playwright-report/results.xml' }]
